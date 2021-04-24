d949f2be03efe5c149627135874c04672176915c
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-04-23 20:09:12 -0500

    errors

--- src/App/index.js
+++ src/App/index.js
@@ -1,36 +1,58 @@
 import React, { useState } from 'react';
 import {
-  Form, FormGroup, Input
+  Form, FormGroup, Input, Button
 } from 'reactstrap';
 import weatherData from '../helpers/data/weatherData';
-import WeatherCard from '../Components/WeatherApp';
+// import WeatherCard from '../Components/WeatherApp';
 import './App.scss';
 
 function App() {
-  const [weatherArray, setWeatherArray] = useState([]);
-  const handleEnterPress = (e) => {
-    if (e.charCode === 13) {
-      e.preventDefault();
-      const city = e.target.value;
-      weatherData(city).then((response) => {
-        weatherArray.push(response);
-        setWeatherArray([...weatherArray]);
+  const [weather, setWeather] = useState('');
+  const [userInput, setUserInput] = useState({
+    city: ''
+  });
+
+  const getWeather = () => {
+    weatherData(userInput.city)
+      .then((temp) => {
+        setWeather(temp);
       });
-    }
+    setWeather({
+      city: userInput.city
+    });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUserInput({
+      city: ''
+    });
+    getWeather();
+  };
+
+  const handleUserInput = (e) => {
+    setWeather({
+      ...weather,
+      [e.target.id]: e.target.value
+    });
+  };
+
   return (
-    <div className='App'>
+      <div className='App'>
       <header>
         <h1 className='header-text'>Do you need a jacket?</h1>
       </header>
-        <WeatherCard weatherArray={weatherArray}/>
-        <div className='form-container'>
-          <Form>
-            <FormGroup>
-                <Input type="text" name="city" id="city" placeholder="Enter city name" onKeyPress={handleEnterPress}/>
+      <div>
+        <Form className="form" onSubmit={handleSubmit}>
+          <FormGroup>
+                <Input type="text" name="city" id="city" placeholder="Enter city name" onChange={handleUserInput}></Input>
             </FormGroup>
-          </Form>
-        </div>
+         <Button type='submit'>
+          Submit
+        </Button>
+       </Form>
+       <div id="weatherContainer">{weather}</div>
+      </div>
     </div>
   );
 }
